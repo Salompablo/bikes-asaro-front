1ae100072ee208e9265a85ae8aaafd0df52e0f66
Salompablo <94395863+Salompablo@users.noreply.github.com>
2026-07-16 12:14:03 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.bikesasaro.com/api/v1',
+  apiUrl: 'https://api.bikesasaro.com.ar/api/v1',
   googleClientId: '107671570936-05qogegv0t5nbmj09vmcnsv3bpji4u1l.apps.googleusercontent.com',
   recaptchaSiteKey: '6Ld--hMtAAAAALpe4rZrZObV120aoIVA38CwulvZ',
 };
